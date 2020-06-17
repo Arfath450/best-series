0952cfb990e7a6e03122a5e2600c5d35c0eed58e
Arfath450 <46299355+Arfath450@users.noreply.github.com>
2020-06-17 15:01:52 +0530

    Updates

---
+++ precache-manifest.608b3b8e793f690304f64ad173872f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217113ae630f57a89b69b4964aaa3970",
+    "url": "/best-series/index.html"
+  },
+  {
+    "revision": "86aa13863a4d3a36d47a",
+    "url": "/best-series/static/css/main.820bed2b.chunk.css"
+  },
+  {
+    "revision": "56947a174ee7fc1259b9",
+    "url": "/best-series/static/js/2.ebdaf5b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/best-series/static/js/2.ebdaf5b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86aa13863a4d3a36d47a",
+    "url": "/best-series/static/js/main.e05d7818.chunk.js"
+  },
+  {
+    "revision": "88224311193433d21492",
+    "url": "/best-series/static/js/runtime-main.4da5e3da.js"
+  }
+]);
